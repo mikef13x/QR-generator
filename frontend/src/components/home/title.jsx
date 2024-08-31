16349c7c1e00fbe1bc768867147131e55510cf1c
mikef13x <mikef132044@gmail.com>
2024-08-31 17:37:29 -0400

    created working QR generator using the npm package

---
+++ frontend/src/components/home/title.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+
+
+
+export default function Title() {
+    return(
+
+            <Box sx={{textAlign: 'center', marginBottom: '50px', marginTop: '100px'}}>
+                <h1>QR Code Generator</h1>
+                <h3>Enter a link and click &quot;Generate&quot; to create a QR Code</h3>
+                <h3>Use &quot;Copy Link&quot; to add the link to your clipboard</h3>
+                <h3>Press &quot;Reset&quot; to clear the input field and start over</h3>
+            </Box>
+    )
+}
