2eff34d2772e1f35ab181ad40984de6cd6a8b467
mikef13x <mikef132044@gmail.com>
2024-10-01 21:38:12 -0400

    changed server js again

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/index.html'));
+      res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
   }
 
